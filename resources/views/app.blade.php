2b50ccc2d038938a13f9e2f573ef1d18b4901701
StrangerTwo <h.volhejn@gmail.com>
2022-02-03 22:05:17 +0100

    add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <title>Pardubický kraťas</title>
     <link href="/css/global.css" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
     <link rel="preconnect" href="https://fonts.googleapis.com">
