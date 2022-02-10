fe392510d3e119818fb5276c6bd1041229d760c3
StrangerTwo <h.volhejn@gmail.com>
2022-02-10 22:37:58 +0100

    links and rules for application

--- resources/js/components/ApplicationPoints.jsx
+++ resources/js/components/ApplicationPoints.jsx
@@ -8,11 +8,11 @@ const ApplicationPoints = () => {
         <ol className={style.applicationPoints}>
             <li>
                 <span>Vyplň přihlášku</span>
-                <p>Stáhni a následně <i>vyplň přihlášku</i></p>
+                <p>Stáhni a následně <i><a href="/kratas_prihlaska.pdf">vyplň přihlášku</a></i></p>
             </li>
             <li>
                 <span>Pošli své dílo</span>
-                <p>Datové soubory společně s elektronickou verzí přihlášky filmu zasílej prostřednictvím datové služby (např. přes www.uschovna.cz) na e-mail <b>info@pardubickykratas.cz</b></p>
+                <p>Datové soubory společně s elektronickou verzí přihlášky filmu zasílej prostřednictvím datové služby (např. přes <a href="https://www.uschovna.cz/">www.uschovna.cz</a>) na e-mail <b>info@pardubickykratas.cz</b></p>
             </li>
         </ol>
     )
