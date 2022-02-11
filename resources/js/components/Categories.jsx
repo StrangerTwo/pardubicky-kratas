c999c06289284a553dadd967ccc5a35588ad2717
StrangerTwo <h.volhejn@gmail.com>
2022-02-11 22:17:35 +0100

    change categories order

--- resources/js/components/Categories.jsx
+++ resources/js/components/Categories.jsx
@@ -23,22 +23,22 @@ const Categories = () => {
 
     return (
         <ul className={style.categories}>
-            <Category title="Snímky studentů filmových škol do 21 let">
-                Text o XXXX
-            </Category>
-            <Category title="Juniorské filmové okénko">
-                Text o XXXX
-            </Category>
-            <Category title="Videoklip a YouTube videoprezentace">
-                Text o XXXX
-            </Category>
-            <Category title="Animovaný film">
+            <Category title="Krátký film na regionální téma města Pardubice a Pardubického kraje">
                 Text o XXXX
             </Category>
             <Category title="Krátký hraný film">
                 Text o XXXX
             </Category>
-            <Category title="Krátký film na regionální téma města Pardubice a Pardubického kraje">
+            <Category title="Animovaný film">
+                Text o XXXX
+            </Category>
+            <Category title="Videoklip a YouTube videoprezentace">
+                Text o XXXX
+            </Category>
+            <Category title="Juniorské filmové okénko">
+                Text o XXXX
+            </Category>
+            <Category title="Snímky studentů filmových škol do 21 let">
                 Text o XXXX
             </Category>
         </ul>
