f1b6c58ffe0038a2625e062b36a705b35bba56b6
StrangerTwo <h.volhejn@gmail.com>
2022-02-16 09:48:59 +0100

    g tag

--- layouts/index.jsx
+++ layouts/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from 'next/head'
 
 import Navigation from "../components/Navigation"
 
@@ -7,6 +8,24 @@ const Layout = ({children}) => {
     return (
         <>
         
+        <Head>
+            <meta name="description" content="Pardubický kraťas je nová studentská filmová soutěž studentů středních škol a druhého stupně základních škol s cílem motivovat studenty k mediální tvůrčí činnosti a obohatit kulturní nabídku pardubického regionu."/>
+            {/* Global Site Tag (gtag.js) - Google Analytics */}
+            <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=G-X1PKZBGEED`}
+            />
+            <script
+                dangerouslySetInnerHTML={{
+                __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', 'G-X1PKZBGEED');
+                `,
+                }}
+            />
+        </Head>
         <Navigation />
         <main>
             <div>
