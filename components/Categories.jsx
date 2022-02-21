ee5e3472df97bcc40677530daa8386579723fe70
StrangerTwo <h.volhejn@gmail.com>
2022-02-21 09:55:45 +0100

    propozice

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,5 +1,7 @@
 import React, {useState} from 'react';
 
+import Link from 'next/link'
+
 import style from "../styles/Home.module.css"
 
 const Category = ({title, children}) => {
@@ -22,14 +24,19 @@ const Category = ({title, children}) => {
 const Categories = () => {
 
     return (
-        <ul className={style.categories}>
-            <li>Krátký film na regionální téma města Pardubice a Pardubického kraje</li>
-            <li>Krátký hraný film</li>
-            <li>Animovaný film</li>
-            <li>Videoklip a YouTube videoprezentace</li>
-            <li>Juniorské filmové okénko</li>
-            <li>Snímky studentů filmových škol do 21 let</li>
-        </ul>
+        <div className={style.categories}>
+            <ul>
+                <li>Krátký film na regionální téma města Pardubice a Pardubického kraje</li>
+                <li>Krátký hraný film</li>
+                <li>Animovaný film</li>
+                <li>Videoklip a YouTube videoprezentace</li>
+                <li>Juniorské filmové okénko (pro žáky 2. stupně ZŠ)</li>
+                <li>Snímky studentů filmových škol do 21 let</li>
+            </ul>
+            <Link href="/kratas_propozice.pdf" passHref>
+                <a className={style.propozice}><i>Propozice soutěže</i></a>
+            </Link>
+        </div>
     )
 }
 
